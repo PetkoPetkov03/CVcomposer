31757df7b2eb634717c1897bd577aca5b78d08ce
Petko Petkov <petko@debian.Petko.com>
2024-07-19 14:19:06 +0300

    main page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,36 @@
 @extends('templates/base')
 
 @section('content')
-    <h1>Hello World!</h1>
-    <h2>{{ time() }}</h2>
+    <div class="form">
+        <h1>Създаване на CV</h1>
+        <div class="inputs">
+            <input type="text" name="first name" placeholder="Име">
+            <input type="text" name="middle name" placeholder="Презиме">
+            <input type="text" name="last name" placeholder="Фамилия">
+        </div>
+
+        <div class="mini-form">
+            <h1>Дата на раждане</h1> 
+            <input type="datetime-local" name="date" id="date">
+        </div>
+
+        <div class="picker-container">
+            <select name="unis" class="picker" required>
+                <option value="" disabled selected >Избиране на университет...</option>
+                <option value="id">Varna uni</option>
+            </select>
+
+            <button class="fas fa-pen"></button>
+        </div>
+
+        <div class="picker-container">
+            <p>Умения в технологии (multichoice)</p>
+            <select name="" id="" multiple>
+                <option value="PHP">PHP</option>
+            </select>
+            <button class="fas fa-pen"></button>
+        </div>
+
+        <button>Запис на CV</button>
+    </div>
 @endsection
