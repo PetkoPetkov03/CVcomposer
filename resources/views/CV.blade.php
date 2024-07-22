9f6450dfd3d27ef496855d4313500f25b43e2402
Petko Petkov <petko@debian.Petko.com>
2024-07-22 18:22:50 +0300

    improving interaface

--- resources/views/CV.blade.php
+++ resources/views/CV.blade.php
@@ -1,23 +1,25 @@
 @extends('templates/base')
-
+<link rel="stylesheet" href="{{ asset("css/cv.css") }}">
 @section('content')
     <div class="CV-component">
-        <div class="person-component">
-            <h1>{{ $person->first_name }}</h1>
-            <h1>{{ $person->middle_name }}</h1>
-            <h1>{{ $person->last_name }}</h1>
-            <p>Дата на раждане: {{ $person->date_of_birth }}</p>
-        </div>
-
-        <div class="CV-info">
-            <p>Име на университет: {{ $cv->university->uni_name }}</p>
-            <p>Оценка от университет: {{ $cv->university->grade }}</p>
-
-            <p>
-                Технически стак: 
-            @foreach ($cv->technologies as $tech)
-                {{ $tech->tech_name }},
-            @endforeach</p>
+        <div class="card">
+            <div class="person-component">
+                <h1>{{ $person->first_name }}</h1>
+                <h1>{{ $person->middle_name }}</h1>
+                <h1>{{ $person->last_name }}</h1>
+                <p>Дата на раждане: {{ $person->date_of_birth }}</p>
+            </div>
+    
+            <div class="CV-info">
+                <p>Име на университет: {{ $cv->university->uni_name }}</p>
+                <p>Оценка от университет: {{ $cv->university->grade }}</p>
+    
+                <p>
+                    Технически стак: 
+                @foreach ($cv->technologies as $tech)
+                    {{ $tech->tech_name }},
+                @endforeach</p>
+            </div>
         </div>
     </div>
 @endsection
